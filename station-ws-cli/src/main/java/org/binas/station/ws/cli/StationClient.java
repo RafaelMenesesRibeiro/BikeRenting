18f32dabc0d856c19f2fefd8e17019d558a2385e
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2018-04-11 11:38:30 +0100

    Continued implementation of station-ws-cli

--- station-ws-cli/src/main/java/org/binas/station/ws/cli/StationClient.java
+++ station-ws-cli/src/main/java/org/binas/station/ws/cli/StationClient.java
@@ -1,5 +1,11 @@
 package org.binas.station.ws.cli;
 
+import org.binas.station.ws.*;
+import javax.xml.ws.BindingProvider;
+import static javax.xml.ws.BindingProvider.ENDPOINT_ADDRESS_PROPERTY;
+import java.util.Map;
+import pt.ulisboa.tecnico.sdis.ws.uddi.UDDINaming;
+
 /**
  * Client port wrapper.
  *
@@ -7,13 +13,13 @@ package org.binas.station.ws.cli;
  * wsimport.
  */
 // TODO implement Port Type interface
-public class StationClient { // implements StationPortType {
+public class StationClient implements StationPortType {
 
 	/** WS service */
-	// StationService service = null;
+	StationService service = null;
 
 	/** WS port (port type is the interface, port is the implementation) */
-	// StationPortType port = null;
+	StationPortType port = null;
 
 	/** UDDI server URL */
 	private String uddiURL = null;
@@ -61,54 +67,53 @@ public class StationClient { // implements StationPortType {
 
 	/** Stub creation and configuration */
 	private void createStub() {
-		// if (verbose)
-		// System.out.println("Creating stub ...");
-		// TODO
-		// service = new StationService();
-		// port = service.getStationPort();
-		//
-		// if (wsURL != null) {
-		// if (verbose)
-		// System.out.println("Setting endpoint address ...");
-		// BindingProvider bindingProvider = (BindingProvider) port;
-		// Map<String, Object> requestContext = bindingProvider.getRequestContext();
-		// requestContext.put(ENDPOINT_ADDRESS_PROPERTY, wsURL);
-		// }
+		if (verbose)
+			System.out.println("Creating stub ...");
+		
+		service = new StationService();
+		port = service.getStationPort();
+		
+		if (wsURL != null) {
+			if (verbose) 
+				System.out.println("Setting endpoint address ...");
+			BindingProvider bindingProvider = (BindingProvider) port;
+			Map<String, Object> requestContext = bindingProvider.getRequestContext();
+			requestContext.put(ENDPOINT_ADDRESS_PROPERTY, wsURL);
+		}
 	}
 
 	// remote invocation methods ----------------------------------------------
 
-	// @Override
-	// public StationView getInfo() {
-	// return port.getInfo();
-	// }
-	//
-	// @Override
-	// public void getBina() throws NoBinaAvail_Exception {
-	// port.getBina();
-	// }
-	//
-	// @Override
-	// public int returnBina() throws NoSlotAvail_Exception {
-	// return port.returnBina();
-	// }
+	@Override
+	public StationView getInfo() {
+		return port.getInfo();
+	}
+	
+	@Override
+	public void getBina() throws NoBinaAvail_Exception {
+		port.getBina();
+	}
+	
+	@Override
+	public int returnBina() throws NoSlotAvail_Exception {
+		return port.returnBina();
+	}
 
 	// test control operations ------------------------------------------------
 
-	// @Override
-	// public String testPing(String inputMessage) {
-	// return port.testPing(inputMessage);
-	// }
-	//
-	// @Override
-	// public void testClear() {
-	// port.testClear();
-	// }
-	//
-	// @Override
-	// public void testInit(int x, int y, int capacity, int returnPrize) throws
-	// BadInit_Exception {
-	// port.testInit(x, y, capacity, returnPrize);
-	// }
+	@Override
+	public String testPing(String inputMessage) {
+		return port.testPing(inputMessage);
+	}
+	
+	@Override
+	public void testClear() {
+		port.testClear();
+	}
+	
+	@Override
+	public void testInit(int x, int y, int capacity, int returnPrize) throws BadInit_Exception {
+		port.testInit(x, y, capacity, returnPrize);
+	}
 
 }
