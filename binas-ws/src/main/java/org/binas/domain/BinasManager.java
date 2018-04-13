06f35861febf62a7cad121092cea48226e6701cc
Rafael Ribeiro <rafaelmmribeiro@gmail.com>
2018-04-13 18:30:17 +0100

    Enhanced BinasManager class

--- binas-ws/src/main/java/org/binas/domain/BinasManager.java
+++ binas-ws/src/main/java/org/binas/domain/BinasManager.java
@@ -1,14 +1,20 @@
 package org.binas.domain;
 
+import java.util.List;
+import java.util.ArrayList;
+
+import org.binas.exception.UserException;
+
 public class BinasManager {
 
 	/** Station identifier. */
 	private String id;
 
+	private static List<User> users = new ArrayList<User>();;
+
 	// Singleton -------------------------------------------------------------
 
-	private BinasManager() {
-	}
+	private BinasManager() {  }
 
 	/**
 	 * SingletonHolder is loaded on the first execution of Singleton.getInstance()
@@ -25,7 +31,23 @@ public class BinasManager {
  	public void setId(String id) {
  		this.id = id;
  	}
-	
-	// TODO
 
+ 	public static void addUser(User user) {
+ 		users.add(user);
+ 	}
+
+ 	public static User getUser(String email) throws UserException {
+ 		for (User u : users) {
+ 			if (u.getEmail().equals(email)) { return u; }
+ 		}
+ 		throw new UserException("No user found in BinasManager.");
+ 	}
+
+ 	public boolean userExists(String email) {
+ 		try { 
+ 			this.getUser(email);
+ 			return true;
+ 		}
+ 		catch (UserException ue) { return false; }
+ 	}
 }
